d2ad9114df66696cbca4d4cbe9f9624c80bbf4b2
Levy Santiago <levyssantiago@gmail.com>
2024-02-06 21:40:25 -0300

    feat: first commit

---
+++ prisma/migrations/20240207001746_create_table_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "pools" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "pools_pkey" PRIMARY KEY ("id")
+);
